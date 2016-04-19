480a0daca7cb469f76af7498bf5b0237c93785f9
nsrau <nsrau@live.it>
2016-04-19 11:15:46 +0200

    first commit

---
+++ assets/themes/wp_bootstrap/inc/customizer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * wp_bootstrap Theme Customizer.
+ *
+ * @package wp_bootstrap
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function wp_bootstrap_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'wp_bootstrap_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function wp_bootstrap_customize_preview_js() {
+	wp_enqueue_script( 'wp_bootstrap_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'wp_bootstrap_customize_preview_js' );
